eaae5fc80a1f9d39a8ab27f19357354c0b9c4a64
zjkv <45632514+zjkv@users.noreply.github.com>
2020-11-30 23:02:51 +0100

    Updated app.js, created controllers, models and routes for invoice_position and invoice_status

---
+++ routes/invoice_position.routes.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+  router = express.Router(),
+  invoice_positionController = require('../controllers/invoice_positionController');
+
+// get all invoice_positions
+router.get('/', invoice_positionController.findAll);
+
+// get invoice_position by id
+router.get('/:id', invoice_positionController.findOne);
+
+// create new user
+router.post('/', invoice_positionController.create);
+
+// update invoice_position by id
+router.put('/:id', invoice_positionController.update);
+
+// delete invoice_position by id
+router.delete('/:id', invoice_positionController.delete);
+
+module.exports = router;
---
+++ routes/invoice_status.routes.js
@@ -0,0 +1,20 @@
+const express = require('express'),
+  router = express.Router(),
+  invoice_statusController = require('../controllers/invoice_statusController');
+
+// get all invoice_statuses
+router.get('/', invoice_statusController.findAll);
+
+// get invoice_status by id
+router.get('/:id', invoice_statusController.findOne);
+
+// create new user
+router.post('/', invoice_statusController.create);
+
+// update invoice_status by id
+router.put('/:id', invoice_statusController.update);
+
+// delete invoice_status by id
+router.delete('/:id', invoice_statusController.delete);
+
+module.exports = router;
